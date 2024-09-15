8a5739f5d3030a314de7cf9886235984693b0048
Stanfash <bnnenna2@gmail.com>
2024-09-15 23:21:23 +1000

    JHU Course

--- JHU-Git RScript.R
+++ JHU-Git RScript.R
@@ -7,6 +7,10 @@ print("I needed to congifure my username and user email as well")
 Print("Initiation Complete")
 
 
+
 # for installing a TeX distribution
 # tinytex::install_tinytex()
 # Sys.which('pdflatex')
+
+# git config --global user.email "Your email@gmail.com"
+# git config --global user.name "Username"
